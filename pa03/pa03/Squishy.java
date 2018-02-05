8cfc441eb945a6b1ec7401f4cb4b5ec0be2ef459
DevoraKrischer <devora.krischer@gmail.com>
2018-02-05 09:30:53 -0500

    mine

--- pa03/pa03/Squishy.java
+++ pa03/pa03/Squishy.java
@@ -1,10 +1,16 @@
 package pa03;
+import java.awt.Color;
 
 public class Squishy extends CircleShape{
 	
 	public static void main(String[] args){
 			Squishy squish=new Squishy();
 			System.out.println(squish);
+	}
+	
+	public Squishy(){
+		super();
+		this.color=new Color(0, 255, 255);
 	}
 	
 	public String toString(){
